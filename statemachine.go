877769f93fce19142af15a3deadb5f80f4eb256e
Raffaele Sena <raff367@gmail.com>
2017-03-14 17:59:12 -0700

    Added machine.New() method that creates a new state maching with start and end stage. Reformatted code!

--- statemachine.go
+++ statemachine.go
@@ -1,36 +1,44 @@
 package statemachine
 
 // An implementation of a finite state machine in Go,
-// inspired by David Mertz's article "Charming Python: Using state machines" 
+// inspired by David Mertz's article "Charming Python: Using state machines"
 // (http://www.ibm.com/developerworks/library/l-python-state/index.html)
 
 type Handler func(interface{}) (string, interface{})
 
 type Machine struct {
-    Handlers   map[string]Handler
-    StartState string
-    EndStates  map[string]bool
+	Handlers   map[string]Handler
+	StartState string
+	EndStates  map[string]bool
 }
 
 func (machine *Machine) AddState(handlerName string, handlerFn Handler) {
-    machine.Handlers[handlerName] = handlerFn
+	machine.Handlers[handlerName] = handlerFn
 }
 
 func (machine *Machine) AddEndState(endState string) {
-    machine.EndStates[endState] = true
+	machine.EndStates[endState] = true
+}
+
+func New(start, end string) *Machine {
+	m := Machine{
+		Handlers:   map[string]Handler{},
+		StartState: start,
+		EndStates:  map[string]bool{end: true}}
+	return &m
 }
 
 func (machine *Machine) Execute(cargo interface{}) {
-    if handler, present := machine.Handlers[machine.StartState]; present {
-        for {
-            nextState, nextCargo := handler(cargo)
-            _, finished := machine.EndStates[nextState]
-            if finished {
-                break
-            } else {
-                handler, present = machine.Handlers[nextState]
-                cargo = nextCargo
-            }
-        }
-    }
+	if handler, present := machine.Handlers[machine.StartState]; present {
+		for {
+			nextState, nextCargo := handler(cargo)
+			_, finished := machine.EndStates[nextState]
+			if finished {
+				break
+			} else {
+				handler, present = machine.Handlers[nextState]
+				cargo = nextCargo
+			}
+		}
+	}
 }
